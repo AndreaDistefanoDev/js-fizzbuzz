37065ec8de9d6d03c6ac929efc86297117add28b
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-02-28 17:54:46 +0100

    multipli di 5

--- main.js
+++ main.js
@@ -9,7 +9,13 @@ for (let i = 1; i <= 100; i++){
     if (i%3 == 0) {
         console.log("Fizz");   
     }
+    // 📝 3.Con i numeri multipli di 5 stampo “Fizz” al posto del numero
     else if (i%5 == 0) {
+        console.log("Buzz");
+        
+    }
+    else if (i%3 == 0 && i%5 == 0){
+        console.log("FizzBuzz");
         
     }
     
