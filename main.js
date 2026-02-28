7475274c1d38effb332171e96e42446ccf6d2afd
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-02-28 17:51:45 +0100

    numeri multipli di 3

--- main.js
+++ main.js
@@ -2,8 +2,15 @@
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
-// Inizio a scrivere un programma che stampa i numeri da 1 a 100
+// 📝 1.Inizio a scrivere un programma che stampa i numeri da 1 a 100
 for (let i = 1; i <= 100; i++){
     console.log(i);
+    // 📝 2.Con i numeri multipli di 3 stampo “Fizz” al posto del numero
+    if (i%3 == 0) {
+        console.log("Fizz");   
+    }
+    else if (i%5 == 0) {
+        
+    }
     
 }
